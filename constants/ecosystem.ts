05120bc52f105138e5bdb8a52d68095c3db29c84
Redfacesa <Redfacesa@gmail.com>
2026-06-23 19:04:35 +0200

    updat

--- constants/ecosystem.ts
+++ constants/ecosystem.ts
@@ -8,7 +8,7 @@ export const REDFACE_HUB_URL = 'https://www.redface.co.za';
 export const APP_STORE_URL = 'https://apps.apple.com/us/app/prayer-reminder/id6755526671';
 export const PLAY_STORE_URL = 'https://play.google.com/store/apps/details?id=com.prayer.reminder.app';
 
-export type RedFaceAppId = 'prayer' | 'services' | 'pay' | 'studio' | 'agency' | 'tours';
+export type RedFaceAppId = 'prayer' | 'services' | 'pay' | 'studio' | 'agency' | 'tours' | 'laundry';
 
 export type EcosystemApp = {
   id: RedFaceAppId;
@@ -21,8 +21,9 @@ export const ECOSYSTEM_APPS: EcosystemApp[] = [
   { id: 'services', name: 'RedFace Services', url: 'https://www.redface.co.za' },
   { id: 'pay', name: 'RedFace Pay', url: 'https://www.redfacepay.co.za' },
   { id: 'studio', name: 'RedFace Studio', url: 'https://www.redfacestudio.com' },
-  { id: 'agency', name: 'Red Face Agency', url: 'https://agency.redface.co.za' },
+  { id: 'agency', name: 'Red Face Agency', url: 'https://www.redface.in' },
   { id: 'tours', name: 'Red Face Tours', url: 'https://redfacetours.co.za' },
+  { id: 'laundry', name: 'RF Laundry', url: 'https://rflaundry.co.za' },
 ];
 
 export const ECOSYSTEM_SIBLING_APPS = ECOSYSTEM_APPS.filter((a) => a.id !== 'prayer');
